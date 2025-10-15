1e77b3f5c2afc1037a022900fba55f4170604b11
Monish0210 <monishpatel831@gmail.com>
2025-10-15 09:32:12 +0530

    add version1 trained model on kaggle

--- Model_Training/model.py
+++ Model_Training/model.py
@@ -1,20 +1,13 @@
-# 1_Model_Training/model.py
-
 import torch
 import torch.nn as nn
 from collections import OrderedDict
 
 class SiamUnet(nn.Module):
-    """
-    SiamUnet is a Siamese U-Net architecture for change detection.
-    It takes two images (pre and post-disaster) as input and outputs segmentation masks.
-    """
     def __init__(self, in_channels=3, out_channels_s=2, out_channels_c=5, init_features=16):
         super(SiamUnet, self).__init__()
-
         features = init_features
 
-        # UNet layers for both branches (shared weights)
+        # Encoder
         self.encoder1 = SiamUnet._block(in_channels, features, name="enc1")
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.encoder2 = SiamUnet._block(features, features * 2, name="enc2")
@@ -23,9 +16,9 @@ class SiamUnet(nn.Module):
         self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.encoder4 = SiamUnet._block(features * 4, features * 8, name="enc4")
         self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
-
         self.bottleneck = SiamUnet._block(features * 8, features * 16, name="bottleneck")
 
+        # Decoder
         self.upconv4 = nn.ConvTranspose2d(features * 16, features * 8, kernel_size=2, stride=2)
         self.decoder4 = SiamUnet._block((features * 8) * 2, features * 8, name="dec4")
         self.upconv3 = nn.ConvTranspose2d(features * 8, features * 4, kernel_size=2, stride=2)
@@ -34,10 +27,9 @@ class SiamUnet(nn.Module):
         self.decoder2 = SiamUnet._block((features * 2) * 2, features * 2, name="dec2")
         self.upconv1 = nn.ConvTranspose2d(features * 2, features, kernel_size=2, stride=2)
         self.decoder1 = SiamUnet._block(features * 2, features, name="dec1")
-
         self.conv_s = nn.Conv2d(in_channels=features, out_channels=out_channels_s, kernel_size=1)
-
-        # Siamese classifier layers for change detection
+        
+        # Change Detection Branch
         self.upconv4_c = nn.ConvTranspose2d(features * 16, features * 8, kernel_size=2, stride=2)
         self.conv4_c = SiamUnet._block(features * 16, features * 16, name="conv4_c")
         self.upconv3_c = nn.ConvTranspose2d(features * 16, features * 4, kernel_size=2, stride=2)
@@ -46,11 +38,10 @@ class SiamUnet(nn.Module):
         self.conv2_c = SiamUnet._block(features * 4, features * 4, name="conv2_c")
         self.upconv1_c = nn.ConvTranspose2d(features * 4, features, kernel_size=2, stride=2)
         self.conv1_c = SiamUnet._block(features * 2, features * 2, name="conv1_c")
-
         self.conv_c = nn.Conv2d(in_channels=features * 2, out_channels=out_channels_c, kernel_size=1)
-
+        
     def forward(self, x1, x2):
-        # UNet on x1 (pre-disaster image)
+        # Branch 1 (pre-disaster)
         enc1_1 = self.encoder1(x1)
         enc2_1 = self.encoder2(self.pool1(enc1_1))
         enc3_1 = self.encoder3(self.pool2(enc2_1))
@@ -60,17 +51,20 @@ class SiamUnet(nn.Module):
         dec4_1 = self.upconv4(bottleneck_1)
         dec4_1 = torch.cat((dec4_1, enc4_1), dim=1)
         dec4_1 = self.decoder4(dec4_1)
+
         dec3_1 = self.upconv3(dec4_1)
         dec3_1 = torch.cat((dec3_1, enc3_1), dim=1)
         dec3_1 = self.decoder3(dec3_1)
+
         dec2_1 = self.upconv2(dec3_1)
         dec2_1 = torch.cat((dec2_1, enc2_1), dim=1)
         dec2_1 = self.decoder2(dec2_1)
+
         dec1_1 = self.upconv1(dec2_1)
         dec1_1 = torch.cat((dec1_1, enc1_1), dim=1)
         dec1_1 = self.decoder1(dec1_1)
 
-        # UNet on x2 (post-disaster image)
+        # Branch 2 (post-disaster)
         enc1_2 = self.encoder1(x2)
         enc2_2 = self.encoder2(self.pool1(enc1_2))
         enc3_2 = self.encoder3(self.pool2(enc2_2))
@@ -80,17 +74,20 @@ class SiamUnet(nn.Module):
         dec4_2 = self.upconv4(bottleneck_2)
         dec4_2 = torch.cat((dec4_2, enc4_2), dim=1)
         dec4_2 = self.decoder4(dec4_2)
+        
         dec3_2 = self.upconv3(dec4_2)
         dec3_2 = torch.cat((dec3_2, enc3_2), dim=1)
         dec3_2 = self.decoder3(dec3_2)
+
         dec2_2 = self.upconv2(dec3_2)
         dec2_2 = torch.cat((dec2_2, enc2_2), dim=1)
         dec2_2 = self.decoder2(dec2_2)
+
         dec1_2 = self.upconv1(dec2_2)
         dec1_2 = torch.cat((dec1_2, enc1_2), dim=1)
         dec1_2 = self.decoder1(dec1_2)
 
-        # Siamese branch to detect change
+        # Change Detection Branch
         diff_bottleneck = bottleneck_2 - bottleneck_1
         dec1_c = self.upconv4_c(diff_bottleneck)
         diff_enc4 = enc4_2 - enc4_1
@@ -111,8 +108,7 @@ class SiamUnet(nn.Module):
         diff_enc1 = enc1_2 - enc1_1
         dec5_c = torch.cat((diff_enc1, dec4_c), dim=1)
         dec5_c = self.conv1_c(dec5_c)
-
-        # The model returns three outputs, but we only need the third one (damage classification) for training
+        
         return self.conv_s(dec1_1), self.conv_s(dec1_2), self.conv_c(dec5_c)
 
     @staticmethod
